7e7019388593b60d83f13f2c4ead5d5c048c9315
arupsarkar-sfdc <arup.sarkar@salesforce.com>
2023-10-17 23:33:39 -0400

    Updated process variables

--- app/auth/oauth/tiktok/page.tsx
+++ app/auth/oauth/tiktok/page.tsx
@@ -7,19 +7,20 @@ const login = () => {
     //create a button with the text "Login with TikTok"
     //on click, redirect to the TikTok login page
     //after login, redirect to the app
-    const CLIENT_KEY = 'awd8pbwevaxtfozp'
-    const SERVER_ENDPOINT_REDIRECT = 'https://social-app-dc-7fcbe05af828.herokuapp.com/auth/oauth/tiktok/callback'
+
     const handleLogin = () => {
+        console.log("login with tiktok-key", process.env.CLIENT_KEY);
+        console.log("login with tiktok-SERVER_ENDPOINT_REDIRECT", process.env.SERVER_ENDPOINT_REDIRECT);
         //redirect to TikTok login page
         console.log("login with tiktok")
         const csrfState = Math.random().toString(36).substring(2);
         let url = 'https://www.tiktok.com/v2/auth/authorize/';
 
         // the following params need to be in `application/x-www-form-urlencoded` format.
-        url += '?client_key={CLIENT_KEY}';
+        url += `?client_key=${process.env.CLIENT_KEY}`;
         url += '&scope=user.info.basic';
         url += '&response_type=code';
-        url += '&redirect_uri={SERVER_ENDPOINT_REDIRECT}';
+        url += `&redirect_uri=${process.env.SERVER_ENDPOINT_REDIRECT}`;
         url += '&state=' + csrfState;
         // redirect to the url mentioned above
 
